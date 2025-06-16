158487c7df5baf9f2a4f89f7125a76e8574abb01
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-06-16 15:09:56 +0300

    Testing the use of the Bridge pattern

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { SurfaceDeliveryFactory } from "./factories/SurfaceDeliveryFactory.ts";
 import { AirDeliveryFactory } from "./factories/AirDeliveryFactory.ts";
 import { SeaDeliveryFactory } from "./factories/SeaDeliveryFactory.ts";
+import {StandardDelivery} from "./bridge/StandardDelivery.ts";
+import {LocalDelivery} from "./bridge/LocalDelivery.ts";
+import {ExpressDelivery} from "./bridge/ExpressDelivery.ts";
+import {InternationalDelivery} from "./bridge/InternationalDelivery.ts";
 
 
 const surfaceFactory = new SurfaceDeliveryFactory();
@@ -16,3 +20,10 @@ console.log(surfaceDelivery.describe());
 console.log(airDelivery.describe());
 console.log(seaDelivery.describe());
 
+// Local Standard Delivery
+const localStandardDelivery = new StandardDelivery(new LocalDelivery());
+console.log(localStandardDelivery.deliver());
+
+// International Express Delivery
+const internationalExpressDelivery = new ExpressDelivery(new InternationalDelivery());
+console.log(internationalExpressDelivery.deliver());
