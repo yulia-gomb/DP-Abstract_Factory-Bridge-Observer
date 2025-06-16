dbf9fb9fd9d892a462dff910f4b5d20e2d0b94a0
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-06-16 14:16:33 +0300

    Add testing for created deliveries

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import { SurfaceDeliveryFactory } from "./factories/SurfaceDeliveryFactory.ts";
+import { AirDeliveryFactory } from "./factories/AirDeliveryFactory.ts";
+import { SeaDeliveryFactory } from "./factories/SeaDeliveryFactory.ts";
 
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <p>
-      Hello
-    </p>
-  </div>
-`
+const surfaceFactory = new SurfaceDeliveryFactory();
+const airFactory = new AirDeliveryFactory();
+const seaFactory = new SeaDeliveryFactory();
+
+// Use factories to create delivery objects
+const surfaceDelivery = surfaceFactory.createDelivery("S-123");
+const airDelivery = airFactory.createDelivery("A-456");
+const seaDelivery = seaFactory.createDelivery("SE-789");
+
+console.log(surfaceDelivery.describe());
+console.log(airDelivery.describe());
+console.log(seaDelivery.describe());
 
